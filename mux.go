9c453273758742be874dfc984a891fe3836efa15
Christopher Hunter <hunter.christopher@icloud.com>
2019-03-04 19:08:34 -0800

    Dry a Little and Fmt

--- mux.go
+++ mux.go
@@ -1,44 +1,44 @@
 package trailmux
 
 import (
-  "net/http"
-  "strings"
+	"net/http"
+	"strings"
 )
 
 type Routes map[string]http.Handler
 
 func (routes Routes) Mux() Mux {
-  return NewMux(routes)
+	return NewMux(routes)
 }
 
 type Mux struct {
-  methods map[string]http.Handler
-  paths map[string]http.Handler
+	methods Routes
+	paths   Routes
 
-  NoMatch http.Handler
+	NoMatch http.Handler
 }
 
 func (mux Mux) NoMatchHandler(handler http.Handler) Mux {
-  mux.NoMatch = handler
-  return mux
+	mux.NoMatch = handler
+	return mux
 }
 
 func (mux Mux) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-  for path, handler := range mux.paths {
-    if strings.HasPrefix(req.URL.Path, path) {
-      http.StripPrefix(path, handler).ServeHTTP(res, req)
-      return
-    }
-  }
+	for path, handler := range mux.paths {
+		if strings.HasPrefix(req.URL.Path, path) {
+			http.StripPrefix(path, handler).ServeHTTP(res, req)
+			return
+		}
+	}
 
-  for method, handler := range mux.methods {
-    if req.Method == method {
-      handler.ServeHTTP(res, req)
-      return
-    }
-  }
+	for method, handler := range mux.methods {
+		if req.Method == method {
+			handler.ServeHTTP(res, req)
+			return
+		}
+	}
 
-  mux.NoMatch.ServeHTTP(res, req)
+	mux.NoMatch.ServeHTTP(res, req)
 }
 
 // NewMux sorts routes by method or path
@@ -46,40 +46,48 @@ func (mux Mux) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 // when any path is added or when no method handlers are added.
 // A method not allowed handler is maped to NoMatch when all handlers
 // are HTTP Method strings
-func NewMux(routes map[string]http.Handler) Mux {
-  var mux Mux
-  mux.methods, mux.paths = sortRoutes(routes)
+func NewMux(routes Routes) Mux {
+	var mux Mux
+	mux.methods, mux.paths = sortRoutes(routes)
 
-  if len(mux.paths) != 0 || len(mux.methods) == 0 {
-    mux.NoMatch = http.HandlerFunc(defaultNotFound)
-  } else {
-    mux.NoMatch = http.HandlerFunc(defaultMethodNotAllowed)
-  }
+	if len(mux.paths) != 0 || len(mux.methods) == 0 {
+		mux.NoMatch = http.HandlerFunc(defaultNotFound)
+	} else {
+		mux.NoMatch = http.HandlerFunc(defaultMethodNotAllowed)
+	}
 
-  return mux
+	return mux
 }
 
 func sortRoutes(routes map[string]http.Handler) (map[string]http.Handler, map[string]http.Handler) {
-  methods, paths := make(map[string]http.Handler), make(map[string]http.Handler)
+	methods, paths := make(map[string]http.Handler), make(map[string]http.Handler)
 
-  for key, handler := range routes {
-    switch key {
-    case http.MethodGet, http.MethodHead, http.MethodPost,
-    http.MethodPut, http.MethodPatch, http.MethodDelete,
-    http.MethodConnect, http.MethodOptions, http.MethodTrace:
-      methods[key] = handler
-    default:
-      paths[key] = handler
-    }
-  }
+	for key, handler := range routes {
+		if isMethod(key) {
+			methods[key] = handler
+		} else {
+			paths[key] = handler
+		}
+	}
 
-  return methods, paths
+	return methods, paths
 }
 
 func defaultNotFound(res http.ResponseWriter, req *http.Request) {
-  http.Error(res, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	http.Error(res, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
 
 func defaultMethodNotAllowed(res http.ResponseWriter, req *http.Request) {
-  http.Error(res, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	http.Error(res, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+}
+
+func isMethod(key string) bool {
+	switch key {
+	case http.MethodGet, http.MethodHead, http.MethodPost,
+		http.MethodPut, http.MethodPatch, http.MethodDelete,
+		http.MethodConnect, http.MethodOptions, http.MethodTrace:
+		return true
+	default:
+		return false
+	}
 }
--- mux_test.go
+++ mux_test.go
@@ -1,142 +1,154 @@
 package trailmux_test
 
 import (
-  "bytes"
-  "testing"
-  "net/http"
-  "net/http/httptest"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-  "github.com/crhntr/trailmux"
+	"github.com/crhntr/trailmux"
 )
 
 func TestMuxHappyPaths(t *testing.T) {
-  var (
-    putCalled, getCalled, someOther bool
-  )
+	var (
+		putCalled, getCalled, someOther bool
+	)
 
-  putFn := func(res http.ResponseWriter, req *http.Request) {
-    putCalled = true
-  }
-  getFn := func(res http.ResponseWriter, req *http.Request) {
-    getCalled = true
-  }
-  otherGetFn := func(res http.ResponseWriter, req *http.Request) {
-    someOther = true
-  }
+	putFn := func(res http.ResponseWriter, req *http.Request) {
+		putCalled = true
+	}
+	getFn := func(res http.ResponseWriter, req *http.Request) {
+		getCalled = true
+	}
+	otherGetFn := func(res http.ResponseWriter, req *http.Request) {
+		someOther = true
+	}
 
-  mux := trailmux.Routes{
-    "/some": trailmux.Routes{
-      "/path": trailmux.Routes{
-        http.MethodPost: http.HandlerFunc(putFn),
-      }.Mux(),
-      "/other": trailmux.Routes{
-        http.MethodGet: http.HandlerFunc(otherGetFn),
-      }.Mux(),
-    }.Mux(),
-    http.MethodGet: http.HandlerFunc(getFn),
-  }.Mux()
+	mux := trailmux.Routes{
+		"/some": trailmux.Routes{
+			"/path": trailmux.Routes{
+				http.MethodPost: http.HandlerFunc(putFn),
+			}.Mux(),
+			"/other": trailmux.Routes{
+				http.MethodGet: http.HandlerFunc(otherGetFn),
+			}.Mux(),
+		}.Mux(),
+		http.MethodGet: http.HandlerFunc(getFn),
+	}.Mux()
 
-  defer func() {
-    if !putCalled {
-      t.Error("it should call putFn")
-    }
-    if !getCalled {
-      t.Error("it should call getFn")
-    }
-    if !someOther {
-      t.Error("it should call otherGetFn")
-    }
-  }()
+	defer func() {
+		if !putCalled {
+			t.Error("it should call putFn")
+		}
+		if !getCalled {
+			t.Error("it should call getFn")
+		}
+		if !someOther {
+			t.Error("it should call otherGetFn")
+		}
+	}()
 
-  mux.ServeHTTP(
-    httptest.NewRecorder(),
-    httptest.NewRequest(http.MethodGet, "/", nil),
-  )
-  mux.ServeHTTP(
-    httptest.NewRecorder(),
-    httptest.NewRequest(http.MethodPost, "/some/path", bytes.NewBuffer(nil)),
-  )
-  mux.ServeHTTP(
-    httptest.NewRecorder(),
-    httptest.NewRequest(http.MethodGet, "/some/other/random/path", bytes.NewBuffer(nil)),
-  )
+	mux.ServeHTTP(
+		httptest.NewRecorder(),
+		httptest.NewRequest(http.MethodGet, "/", nil),
+	)
+	mux.ServeHTTP(
+		httptest.NewRecorder(),
+		httptest.NewRequest(http.MethodPost, "/some/path", bytes.NewBuffer(nil)),
+	)
+	mux.ServeHTTP(
+		httptest.NewRecorder(),
+		httptest.NewRequest(http.MethodGet, "/some/other/random/path", bytes.NewBuffer(nil)),
+	)
 }
 
 func TestMuxSadPaths(t *testing.T) {
-  var (
-    putCalled, noMatchCalled bool
-  )
+	var (
+		putCalled, noMatchCalled bool
+	)
 
-  putFn := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-    putCalled = true
-  })
-  getFn := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {})
-  noMatch := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-    res.WriteHeader(http.StatusNotFound)
-    noMatchCalled = true
-  })
+	putFn := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		putCalled = true
+	})
+	getFn := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {})
+	noMatch := http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.WriteHeader(http.StatusNotFound)
+		noMatchCalled = true
+	})
 
-  mux := trailmux.Routes{
-    "/some": trailmux.Routes{
-      "/other": trailmux.Routes{}.Mux(),
-      "/path": trailmux.Routes{
-        http.MethodPost: putFn,
-      }.Mux(),
-    }.Mux(),
-    http.MethodGet: getFn,
-    "/no-match": trailmux.Routes{}.Mux().NoMatchHandler(noMatch),
-  }.Mux()
+	mux := trailmux.Routes{
+		"/some": trailmux.Routes{
+			"/other": trailmux.Routes{}.Mux(),
+			"/path": trailmux.Routes{
+				http.MethodPost: putFn,
+			}.Mux(),
+		}.Mux(),
+		http.MethodGet: getFn,
+		"/no-match":    trailmux.Routes{}.Mux().NoMatchHandler(noMatch),
+	}.Mux()
 
-  defer func() {
-    if putCalled {
-      t.Error("it should not call putFn")
-    }
-  }()
+	defer func() {
+		if putCalled {
+			t.Error("it should not call putFn")
+		}
+	}()
 
-  {
-    res := httptest.NewRecorder()
-    mux.ServeHTTP(
-      res,
-      httptest.NewRequest(http.MethodDelete, "/", nil),
-    )
-    if res.Code != http.StatusNotFound {
-      t.Errorf("expected %d got %d", http.StatusNotFound, res.Code)
-    }
-  }
+	{
+		res := httptest.NewRecorder()
+		mux.ServeHTTP(
+			res,
+			httptest.NewRequest(http.MethodDelete, "/", nil),
+		)
+		if res.Code != http.StatusNotFound {
+			t.Errorf("expected %d got %d", http.StatusNotFound, res.Code)
+		}
+	}
 
-  {
-    res := httptest.NewRecorder()
-    mux.ServeHTTP(
-      res,
-      httptest.NewRequest(http.MethodDelete, "/some/path", nil),
-    )
-    if res.Code != http.StatusMethodNotAllowed {
-      t.Errorf("expected %d got %d", http.StatusMethodNotAllowed, res.Code)
-    }
-  }
+	{
+		res := httptest.NewRecorder()
+		mux.ServeHTTP(
+			res,
+			httptest.NewRequest(http.MethodDelete, "/some/path", nil),
+		)
+		if res.Code != http.StatusMethodNotAllowed {
+			t.Errorf("expected %d got %d", http.StatusMethodNotAllowed, res.Code)
+		}
+	}
 
-  {
-    res := httptest.NewRecorder()
-    mux.ServeHTTP(
-      res,
-      httptest.NewRequest(http.MethodGet, "/some/other", nil),
-    )
-    if res.Code != http.StatusNotFound {
-      t.Errorf("expected %d got %d", http.StatusNotFound, res.Code)
-    }
-  }
+	{
+		res := httptest.NewRecorder()
+		mux.ServeHTTP(
+			res,
+			httptest.NewRequest(http.MethodGet, "/some/other", nil),
+		)
+		if res.Code != http.StatusNotFound {
+			t.Errorf("expected %d got %d", http.StatusNotFound, res.Code)
+		}
+	}
 
-  {
-    res := httptest.NewRecorder()
-    mux.ServeHTTP(
-      res,
-      httptest.NewRequest(http.MethodGet, "/no-match", nil),
-    )
-    if res.Code != http.StatusNotFound {
-      t.Errorf("expected %d got %d", http.StatusNotFound, res.Code)
-    }
-    if !noMatchCalled {
-      t.Error("it should not call noMatch")
-    }
-  }
+	{
+		res := httptest.NewRecorder()
+		mux.ServeHTTP(
+			res,
+			httptest.NewRequest(http.MethodGet, "/no-match", nil),
+		)
+		if res.Code != http.StatusNotFound {
+			t.Errorf("expected %d got %d", http.StatusNotFound, res.Code)
+		}
+		if !noMatchCalled {
+			t.Error("it should not call noMatch")
+		}
+	}
 }
+
+// func TestMux_Validate(t *testing.T) {
+// 	mux := trailmux.Routes{
+// 		http.MethodGet: trailmux.Routes{
+// 			"/path": trailmux.Routes{}.Mux(),
+// 		}.Mux(),
+// 	}.Mux()
+//
+// 	if err := mux.Validate(); err == nil {
+// 		t.Errorf("expected an error")
+// 	}
+// }
